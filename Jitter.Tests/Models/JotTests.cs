15782951de61f85865aee4123b3d52025a546328
Shelley Davis-Wise <shelleydaviswise@gmail.com>
2015-11-18 12:01:20 -0600

    Jot models with Trivial Tests

---
+++ Jitter.Tests/Models/JotTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jitter.Models;
+
+namespace Jitter.Tests.Models
+{
+    [TestClass]
+    public class JotTests
+    {
+        [TestMethod]
+        public void JotEnsureICanCreateInstance()
+        {//bootstrapping test to see that all the pieces are connected
+            Jot a_jot = new Jot();
+            Assert.IsNotNull(a_jot);
+        }//simple test to gauge the presence of a class
+
+        [TestMethod]
+        public void JotEnsureJotHasAllTheThings()
+        {
+            //Arrange
+            Jot a_jot = new Jot();
+
+            //Act
+            DateTime expected_time = DateTime.Now;
+
+            a_jot.JotId = 1;
+            a_jot.Content = "My Content";
+            a_jot.Date = expected_time;
+            a_jot.Author = null;
+                //stopped here to create a JitterUser class to make a Jot author will need to define later
+            a_jot.Picture = "https://google.com";
+               
+                //Assert
+            Assert.AreEqual(1, a_jot.JotId);
+            Assert.AreEqual("My Content", a_jot.Content );
+            Assert.AreEqual(expected_time, a_jot.Date);
+            Assert.AreEqual(null, a_jot.Author);
+            Assert.AreEqual("https://google.com", a_jot.Picture);
+           
+        }
+
+        [TestMethod]
+        public void JotEnsureICanUseObjectInitializerSyntax()
+        {
+            //Arrange
+            DateTime expected_time = DateTime.Now;
+            //Act
+            Jot a_jot = new Jot { JotId = 2, Content = "My Content", Date = expected_time, Author = null, Picture = "https://google.com" };
+
+            //Assert
+            Assert.AreEqual(2, a_jot.JotId);
+            Assert.AreEqual("My Content", a_jot.Content);
+            Assert.AreEqual(expected_time, a_jot.Date);
+            Assert.AreEqual(null, a_jot.Author);
+            Assert.AreEqual("https://google.com", a_jot.Picture);
+
+        }
+    }
+    //Arrange
+    //Act
+    //Assert
+
+}
